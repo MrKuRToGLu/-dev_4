ea588c409efc440cef019554c884f553893850f8
User <User@USER-BILGISAYAR>
2022-10-23 14:52:30 +0300

    Add project files.

---
+++ ödev_4/Program.cs
@@ -0,0 +1,340 @@
+﻿using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ödev_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            {
+                string kullanıcı = "ADMiN", sifre = "12345";
+                int h;
+
+                //Console.WriteLine("Kullanıcı Adını Giriniz");
+                //ullanıcı = Console.ReadLine();
+
+                //onsole.WriteLine("Şifre Giriniz");
+                //ifre = (Console.ReadLine());
+
+                for
+                   (h = 3; h > 0; h--)
+                {
+                    Console.WriteLine("Kullanıcı Adını Giriniz");
+                    kullanıcı = Console.ReadLine();
+                    Console.WriteLine("Şifre Giriniz");
+                    sifre = (Console.ReadLine());
+
+
+                    if
+                          (kullanıcı == "ADMiN" && sifre == "1234")
+                    {  
+                        Console.WriteLine("HOŞ GELDİNİZ");
+                        Console.Read();
+                    }
+
+                    
+
+                   
+
+
+                    Console.WriteLine("Kullanıcı veya Şifre Yanlış");
+                    Console.WriteLine($"Kalan hakkınız{h - 1} ");
+
+
+
+
+
+
+
+                }
+
+
+                {
+                    Console.WriteLine($"Şifreniz Bloke Olmuştur");
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //Console.WriteLine("Şifre Giriniz");
+                //ifre = (Console.ReadLine());
+
+
+                //h > 0)
+
+                //ontinue;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {
+                    //onsole.WriteLine($"{h - 1}Kullanıcı Giriniz");
+                    //ifre = (Console.ReadLine());
+                    //     Console.WriteLine($"{h-1}Şifre Giriniz");
+                    //sifre = (Console.ReadLine());
+
+                    //f (kullanıcı == "ADMiN" || sifre == "1234")
+
+                    //continue;
+                    //-;
+
+                    //Console.WriteLine("Hoş Geldiniz");
+
+
+
+
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                /// if (kullanıcı == "ADMiN" && sifre == "1234") 
+
+                // continue;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
